7a204f53d29906fe4007a0a04433ef06b95fa117
Nicrex <111898535+Nicrex@users.noreply.github.com>
2023-03-19 17:54:06 +0100

    Update install.ps1

--- files/install.ps1
+++ files/install.ps1
@@ -1,1 +1,2 @@
-Start-Process "C:\7z2201-x64.exe" -ArgumentList "/S /v/qn"
+#Start-Process "C:\7z2201-x64.exe" -ArgumentList "/S /v/qn"
+Start-Process "C:\7z2201-x64.exe" -Argument "/Silent" -PassThru
